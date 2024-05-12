eb09269e735112b3fc61e752222af2334677532f
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-05-13 01:18:55 +0700

    add: middleware JWT

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -1,10 +1,22 @@
-
+const {verify} = require('jsonwebtoken')
 
 const userMiddleware = (req, res, next) => {
-  const credentials = req.headers;
+  try {
+    const credentials = req.headers;
+  if (!credentials.authorization) {
+    return res.status(401).json({message: 'Unauthorized!'});
+  }
+  token = credentials.authorization.split(' ')[1];
+  const decoded = verify(token, process.env.JWT_SECRET_KEY);
+  console.log(decoded);
   console.log(credentials.authorization);
+
   console.log('Middleware executed');
   next()
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 }
 
 module.exports = {
--- backend/src/routers/chattingRoute.js
+++ backend/src/routers/chattingRoute.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const {userMiddleware} = require('../middleware/userMiddleware');
 const chattingController = require('../controllers/chattingController');
 
-router.get('/get-conversation/:conversation_id', chattingController.getConversation);
+router.get('/get-conversation/:conversation_id', userMiddleware,chattingController.getConversation);
 router.get('/list-conversations/:user_id', chattingController.getListOfConversations);
 router.post('/send-message', chattingController.sendMessage);
 router.post('/create-conversation/:user_id', chattingController.createNewConversation);
