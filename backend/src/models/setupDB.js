611c9bdee8b413a2deb192e0d772a373c7429ba9
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-05-25 08:59:08 +0700

    update

--- backend/src/models/setupDB.js
+++ backend/src/models/setupDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const InitDB = () =>{
-    const dbUrl = process.env.DB_URL;
+    const dbUrl = process.env.DB_DEV_URL;
     mongoose.connect(dbUrl);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
--- backend/src/routers/chattingRoute.js
+++ backend/src/routers/chattingRoute.js
@@ -3,8 +3,8 @@ const {userMiddleware} = require('../middleware/userMiddleware');
 const chattingController = require('../controllers/chattingController');
 
 router.get('/get-conversation/:conversation_id', userMiddleware,chattingController.getConversation);
-router.get('/list-conversations/:user_id', chattingController.getListOfConversations);
-router.post('/send-message', chattingController.sendMessage);
-router.post('/create-conversation/:user_id', chattingController.createNewConversation);
+router.get('/list-conversations/:user_id', userMiddleware,  chattingController.getListOfConversations);
+router.post('/send-message', userMiddleware,chattingController.sendMessage);
+router.post('/create-conversation/:user_id', userMiddleware,chattingController.createNewConversation);
 
 module.exports = router;
