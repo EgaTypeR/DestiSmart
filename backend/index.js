1e4b3e5acf0b9a53bd53d8c89143d9a7812ec796
EgaTypeR <ega.rizky.setiawan@mail.ugm.ac.id>
2024-05-27 09:57:10 +0700

    ser gpt 3.5

--- backend/index.js
+++ backend/index.js
@@ -17,23 +17,11 @@ const ChattingRouter = require('./src/routers/chattingRoute')
 const AuthRouter = require('./src/routers/authRoute')
 const DistanceRouter = require('./src/routers/distanceRoute')
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ["http://localhost:3000", "https://desti-smart.vercel.app"];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow cookies to be sent with requests
-  optionsSuccessStatus: 204
-};
+
 
 const app = express()
 SetupDB()
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(helmet())
 app.use(express.json({
   limit: '15kb'
